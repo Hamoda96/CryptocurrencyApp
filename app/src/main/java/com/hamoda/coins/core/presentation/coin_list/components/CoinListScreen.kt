2898f9e294bc3154ec3b38669048394fad796183
Mahmoud Hamoda <31393608+Hamoda96@users.noreply.github.com>
2023-07-12 13:15:56 +0300

    Dev (#8)
    * add crashlytics dependancy
    
    * google service
    
    * Add/githup action ci (#3)
    
    * add crashlytics dependancy (#2)
    
    * add crashlytics dependancy
    
    * google service
    
    * upgrade wrapper file
    
    * add first githup action ci file
    
    * update first githup action ci file
    
    * update first githup action ci file
    
    * fix format test githup action (#5)
    
    * Test/githup action (#7)
    
    * fix format test githup action
    
    * update GitHup action add s to runs

--- app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListScreen.kt
+++ app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListScreen.kt
@@ -2,21 +2,12 @@ package com.plcoding.cryptocurrencyappyt.presentation.coin_list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.hamoda.coins.core.presentation.coin_list.components.CoinListItem
+import com.hamoda.coins.core.presentation.coin_list.components.CoinListViewModel
 
 @Composable
 fun CoinListScreen(
