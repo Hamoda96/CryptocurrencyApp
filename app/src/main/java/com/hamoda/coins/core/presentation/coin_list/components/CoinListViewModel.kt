eab3a965eb73e7691ddbea753623af2086f66a91
mahmoudhamoda <mahmoud.m.hamoda@gmail.com>
2023-07-12 13:16:35 +0300

    fix format

--- app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListViewModel.kt
+++ app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListViewModel.kt
@@ -14,6 +14,7 @@ class CoinListViewModel(
 
     private val _state = mutableStateOf(CoinListState())
     val state: State<CoinListState> = _state
+
     init {
         MainScope().launch {
             getCoins()
