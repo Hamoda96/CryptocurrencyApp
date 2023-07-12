641a4f3956f9691c53bfea1be6df5408d7ec25d1
Mahmoud Hamoda <31393608+Hamoda96@users.noreply.github.com>
2023-07-12 12:49:51 +0300

    fix format test githup action (#5)

--- app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListViewModel.kt
+++ app/src/main/java/com/hamoda/coins/core/presentation/coin_list/components/CoinListViewModel.kt
@@ -3,24 +3,15 @@ package com.plcoding.cryptocurrencyappyt.presentation.coin_list
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.hamoda.coins.common.Resource
-import com.hamoda.coins.core.domain.model.coins.Coins
 import com.hamoda.coins.core.domain.use_case.get_coins.GetCoinsUseCase
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import org.koin.androidx.scope.ScopeActivity
 
 class CoinListViewModel(
     private val getCoinsUseCase: GetCoinsUseCase
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(
-        CoinListState(
-        )
-    )
+    private val _state = mutableStateOf(CoinListState())
     val state: State<CoinListState> = _state
 
     init {
